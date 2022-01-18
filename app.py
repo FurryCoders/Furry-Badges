76418be1be85a39395863e22385e2fb8e8093a70
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-01-18 13:09:51 +0100

    app,static - support multiple logos

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from fastapi.responses import RedirectResponse
 from fastapi.responses import Response
 
 root_folder: Path = Path(__file__).parent
-static_folder: Path = root_folder / "static"
+logos_folder: Path = root_folder / "static" / "logos"
 
 app: FastAPI = FastAPI(servers=[{"url": "https://furaffinity-badge.herokuapp.com"}],
                        license_info={"name": "European Union Public Licence v. 1.2", "url": "https://eupl.eu/1.2/en"},
@@ -17,14 +17,16 @@ app: FastAPI = FastAPI(servers=[{"url": "https://furaffinity-badge.herokuapp.com
 
 app.add_route("/", lambda r: RedirectResponse("https://github.com/MatteoCampinoti94/furaffinity-badge"), ["GET"])
 
+
 badge: dict[str, str | int] = {
     "schemaVersion": 1,
     "label": "FurAffinity",
     "message": "Badge",
     "color": "#FAAF3A",
     "labelColor": "#151718",
-    "logoSvg": (static_folder / "logo.svg").read_text()
 }
+
+logos: dict[str, str] = {p.name.removesuffix(".svg"): p.read_text() for p in logos_folder.iterdir()}
 
 
 @cache
@@ -39,12 +41,12 @@ def get_badge(endpoint: str, **params) -> Response:
 
 
 @app.get("/badge/endpoint/", response_class=ORJSONResponse)
-@app.get("/badge/endpoint/{username}", response_class=ORJSONResponse)
-def badge_endpoint(request: Request, username: str = None):
-    return badge | {"message": username or badge["message"]} | {**request.query_params}
+@app.get("/badge/endpoint/{site}/{username}", response_class=ORJSONResponse)
+def badge_endpoint(request: Request, site: str = None, username: str = None):
+    return badge | {"message": username or badge["message"], "logoSvg": logos[site.lower()]} | {**request.query_params}
 
 
-@app.get("/badge/user/{username}", response_class=Response)
-def badge_user(request: Request, username: str):
-    return get_badge(app.servers[0]["url"] + app.url_path_for(badge_endpoint.__name__, username=username),
+@app.get("/badge/user/{site}/{username}", response_class=Response)
+def badge_user(request: Request, site: str, username: str):
+    return get_badge(app.servers[0]["url"] + app.url_path_for(badge_endpoint.__name__, site=site, username=username),
                      **request.query_params)
