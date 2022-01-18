3e41dc845367f18c8bba6b8a74cef016af397358
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-01-18 18:39:22 +0100

    app - allow setting label via path

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ root_folder: Path = Path(__file__).parent
 logos_folder: Path = root_folder / "static" / "logos"
 templates: Jinja2Templates = Jinja2Templates(str(root_folder))
 
-badge: dict[str, str | int] = {
+badge_template: dict[str, str | int] = {
     "schemaVersion": 1,
     "label": "Furry",
     "message": "Badge",
@@ -29,7 +29,9 @@ app: FastAPI = FastAPI(servers=[{"url": "https://furry-badges.herokuapp.com"}],
                        license_info={"name": "European Union Public Licence v. 1.2", "url": "https://eupl.eu/1.2/en"},
                        docs_url=None, redoc_url=None)
 
-app.add_route("/", lambda r: templates.TemplateResponse("index.html", {"request": r, "sites": [*logos.keys()]}), ["GET"])
+app.add_route("/",
+              lambda r: templates.TemplateResponse("index.html", {"request": r, "sites": [*logos.keys()]}),
+              ["GET"])
 
 
 @cache
@@ -43,15 +45,19 @@ def get_badge(endpoint: str, **params) -> Response:
     )
 
 
-@app.get("/badge/endpoint/{site}/{username}", response_class=ORJSONResponse)
-def badge_endpoint(username: str, site: str = None):
+@app.get("/badge/endpoint/{site}/{username}/", response_class=ORJSONResponse)
+@app.get("/badge/endpoint/{site}/{username}/{label}/", response_class=ORJSONResponse)
+def badge_endpoint(site: str, username: str, label: str = None):
     site_: str = site.lower()
+    badge_label: dict = {"label": label or site_, "message": username}
     badge_colors: dict = {"labelColor": colors[site_][0], "color": colors[site_][1]} if site_ in colors else {}
-    badge_logo: dict = {"logoSvg": logos[site_]} if site_ in logos else {}
-    return badge | {"label": site, "message": username} | badge_colors | badge_logo
+    return badge_template | badge_label | badge_colors | badge_logo
 
 
-@app.get("/badge/user/{site}/{username}", response_class=Response)
-def badge_user(request: Request, site: str, username: str):
-    return get_badge(app.servers[0]["url"] + app.url_path_for(badge_endpoint.__name__, site=site, username=username),
+@app.get("/badge/user/{site}/{username}/", response_class=Response)
+@app.get("/badge/user/{site}/{username}/{label}/", response_class=Response)
+def badge_user(request: Request, site: str, username: str, label: str = None):
+    return get_badge(app.servers[0]["url"] +
+                     app.url_path_for(badge_endpoint.__name__, site=site, username=username,
+                                      **({"label": label} if label else {})),
                      **request.query_params)
