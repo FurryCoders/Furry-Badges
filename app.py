4f71cef88b3eb5c2bc23a429327592f08f2a6456
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-01-18 18:56:43 +0100

    app - fix endpoint error (missing variable)

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def badge_endpoint(site: str, username: str, label: str = None):
     site_: str = site.lower()
     badge_label: dict = {"label": label or site_, "message": username}
     badge_colors: dict = {"labelColor": colors[site_][0], "color": colors[site_][1]} if site_ in colors else {}
+    badge_logo: dict = {"logoSvg": logos[site_]} if site_ in logos else {}
     return badge_template | badge_label | badge_colors | badge_logo
 
 
